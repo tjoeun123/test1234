23df826bbd1cd2c16e4a27ffa93bd6d1a9f1736c
tjoeun123 <DavidandLivingstone@gmail.com>
2024-05-20 12:00:14 +0900

    first commit

---
+++ src/test/java/com/tjoeun/shop/Test1234ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1234ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
